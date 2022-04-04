7e345df68870928b017ca34ca1d97d70b65f8546
amishajha <jhaamisha096@gmail.com>
2022-04-04 22:40:12 +0530

    new file added

---
+++ leetcode java/SelectBuildings.java
@@ -0,0 +1,34 @@
+class SelectBuildings{
+	
+	public void main(String[] args) {
+		
+		
+		String s  =  "11100";
+		System.out.print(numberOfWays(s));
+		
+		
+	}
+	
+	
+	
+	
+	 public long numberOfWays(String s) {
+		 long one = 0, zero = 0, oneZero = 0, zeroOne = 0, ways = 0;
+	        for (int i = 0; i < s.length(); ++i) {
+	            if (s.charAt(i) == '0') {
+	                ++zero;
+	                oneZero += one; // Count in '10'.
+	                ways += zeroOne; // Count in '010'.
+	            }else {
+	                ++one;
+	                zeroOne += zero; // Count in '01'.
+	                ways += oneZero; // Count in '101'.
+	            }
+	        }
+	        return ways;
+	    }
+	
+	
+	
+	
+}
